0c0ff7e82b069dde9ce11a33cbce9dc3fde5a1a8
Barun Halder <barunhalder.kolkata@gmail.com>
2018-03-18 03:44:30 +0530

    Tweaked the params to have 98.4% acc

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -12,43 +12,60 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
+from keras.utils import np_utils
+from keras.optimizers import SGD
+import numpy as np
 
-batch_size = 128
+learning_rate = 0.1
+batch_size = 32
 num_classes = 10
 epochs = 20
+input_dim = 784
+hidden_layer_activation = 512
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
+x_train = x_train.reshape(60000, input_dim )
+x_test = x_test.reshape(10000, input_dim)
+val_indices = np.random.permutation(x_train.shape[0])
+validation_indices, training_indices = val_indices[:10000], val_indices[10000:]
+x_valid = x_train[validation_indices, :]
+x_train = x_train[training_indices, :]
+y_valid = y_train[validation_indices]
+y_train = y_train[training_indices]
 x_train = x_train.astype('float32')
+x_valid = x_valid.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
+x_valid /= 255
 x_test /= 255
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+y_train = np_utils.to_categorical(y_train, num_classes)
+y_valid = np_utils.to_categorical(y_valid, num_classes)
+y_test = np_utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dense(512, activation='relu'))
+model.add(Dense(hidden_layer_activation, activation='relu', input_shape=(input_dim,)))
+model.add(Dense(hidden_layer_activation, activation='relu'))
+model.add(Dense(hidden_layer_activation, activation='relu'))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.summary()
 
+sgd=SGD(lr=learning_rate)
 model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
+              optimizer=sgd,
               metrics=['accuracy'])
 
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
-                    epochs=epochs,
+                    nb_epoch=epochs,
                     verbose=1,
-                    validation_data=(x_test, y_test))
+                    validation_data=(x_valid, y_valid))
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
